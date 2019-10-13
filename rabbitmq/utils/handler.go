20c860bd1abc878afef8d6bc1574bbd15e831299
Myron William Echenim <myron.echenim@live.com>
2019-10-13 01:13:31 -0700

    create json fucntion for packing strruct to string

--- rabbitmq/utils/handler.go
+++ rabbitmq/utils/handler.go
@@ -5,6 +5,17 @@ import (
 	"log"
 )
 
+// //ToJSON function to convert string to json
+// func ToJSON(m struct) []byte {
+
+// 	b, err := json.Marshal(m)
+// 	if err != nil {
+// 		log.Fatalf("%s : %s", err, "Fail to convert to json")
+// 	}
+
+// 	return b
+// }
+
 //ErrorHandler function for capturing error
 func ErrorHandler(err error, errorMessage string) {
 	if err != nil {
