8ca8d3af8adda17d169e69ed5c244f41336f36f9
William Myron  Echenim <myron.echenim@live.com>
2021-01-07 15:40:28 -0800

    Update handler.go

--- rabbitmq/utils/handler.go
+++ rabbitmq/utils/handler.go
@@ -19,7 +19,7 @@ import (
 //ErrorHandler function for capturing error
 func ErrorHandler(err error, errorMessage string) {
 	if err != nil {
-		log.Fatalf("%s : %s", err, errorMessage)
+		
 		fmt.Printf("%v : %v", errorMessage, err)
 	}
 }
