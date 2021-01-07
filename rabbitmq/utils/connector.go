bd511ad81b0ccc3e931223ffd757b624dc907b75
William Myron  Echenim <myron.echenim@live.com>
2021-01-07 15:41:39 -0800

    Update connector.go

--- rabbitmq/utils/connector.go
+++ rabbitmq/utils/connector.go
@@ -5,6 +5,6 @@ import "github.com/streadway/amqp"
 //ConnectRabbitMQ connection function
 func ConnectRabbitMQ() (*amqp.Connection, error) {
 
-	//conn, err := amqp.Dial("amqp://devng:103Solutionx@192.168.1.74:5672")
-	return amqp.Dial("amqp://devng:103Solutionx@192.168.1.74:5672")
+	
+	return amqp.Dial("amqp://devng:#########@****.****.***:5672")
 }
